496757e542a4a0b5a0a6304b2c529ec1df9bc66e
dillon <dillonjaap@gmail.com>
2025-10-08 08:37:50 -0600

    stuff

--- bin/main.ml
+++ bin/main.ml
@@ -10,13 +10,13 @@
 open Base
 open Angstrom
 
-let sexp_parser =
-  let open Char in
-  satisfy (fun ch -> ch = 't' || ch = 'a')
+let sexp_parser = many @@ choice [ char 'a'; char 'b'; char 'c' ]
 
 let () =
   let open Stdlib.Printf in
-  let input = "ta" in
+  let input = "abc" in
   match parse_string ~consume:All sexp_parser input with
-  | Ok result -> printf "%c" result
+  | Ok result ->
+      printf "%s"
+        (List.fold result ~init:"" ~f:(fun acc cur -> acc ^ String.of_char cur))
   | Error msg -> printf "%s" msg
