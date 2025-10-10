c24dc6d676be0004f8a92de4c2d069173afcd921
dillon <dillonjaap@gmail.com>
2025-10-09 19:07:06 -0600

    number

--- bin/main.ml
+++ bin/main.ml
@@ -10,13 +10,39 @@
 open Base
 open Angstrom
 
-let sexp_parser = many @@ choice [ char 'a'; char 'b'; char 'c' ]
+let is_digit = function
+  | '0' .. '9' -> true
+  | _ -> false
+;;
+
+(* let is_non_zero = function *)
+(*   | '1' .. '9' -> true *)
+(*   | _ -> false *)
+(* ;; *)
+
+let number =
+  let* sign = option "" (string "-") in
+  let* int_part = take_while is_digit in
+  let* decimal_part =
+    option ""
+    @@
+    let* dec = string "." in
+    let* digits = take_while is_digit in
+    return (dec ^ digits)
+  in
+  return
+    (match sign ^ int_part ^ decimal_part with
+     | s when String.is_prefix s ~prefix:"." -> "0" ^ s
+     | s -> s)
+  <?> "number"
+;;
+
+let sexp_parser = number
 
 let () =
   let open Stdlib.Printf in
-  let input = "abc" in
+  let input = ".1" in
   match parse_string ~consume:All sexp_parser input with
-  | Ok result ->
-      printf "%s"
-        (List.fold result ~init:"" ~f:(fun acc cur -> acc ^ String.of_char cur))
+  | Ok result -> printf "%s" result
   | Error msg -> printf "%s" msg
+;;
