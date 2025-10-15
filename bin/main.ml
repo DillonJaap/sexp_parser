ccec5cfb3095073d574278c607300bb4bef457fa
dillon <dillonjaap@gmail.com>
2025-10-15 07:42:10 -0600

    working

--- bin/main.ml
+++ bin/main.ml
@@ -1,76 +1,97 @@
-(* S-Expression Formal Grammar *)
-(* 
-  sexpr ::= list | symbol | number
-  list ::= "(" list-rest
-  list-rest ::= ")" | "." sexpr ")" | sexpr list-rest
-  symbol ::= /[^s\]+/
-  number ::= /-?[0-9][1-9]*(\.[0-9]+)?/
-*)
-
-(* TODO: dot notation *)
 open Base
 open Angstrom
+
+(* -------------------------------------------------------------------------- *)
+(* AST *)
+(* -------------------------------------------------------------------------- *)
 
 type sexpr =
   | Number of string
   | Symbol of string
-  | List of sexpr list
+  | Cons of sexpr * sexpr
+  | Null
 [@@deriving show]
+
+(* -------------------------------------------------------------------------- *)
+(* Helpers *)
+(* -------------------------------------------------------------------------- *)
 
 let is_digit = function
   | '0' .. '9' -> true
   | _ -> false
 ;;
 
+let is_delim = function
+  | ' ' | '\t' | '\n' | '\r' | '(' | ')' -> true
+  | _ -> false
+;;
+
+let ws =
+  skip_while (function
+    | ' ' | '\t' | '\n' | '\r' -> true
+    | _ -> false)
+;;
+
+let token p = ws *> p <* ws
+
+(* -------------------------------------------------------------------------- *)
+(* Atom parsers *)
+(* -------------------------------------------------------------------------- *)
 let number =
   let* sign = option "" (string "-") in
-  let* int_part = take_while is_digit in
+  let* int_part = take_while1 is_digit in
   let* decimal_part =
     option ""
-    @@
-    let* dec = string "." in
-    let* digits = take_while is_digit in
-    return (dec ^ digits)
+    @@ let* _ = char '.' in
+       let* digits = take_while1 is_digit in
+       return ("." ^ digits)
   in
-  return
-    (match sign ^ int_part ^ decimal_part with
-     | s when String.is_prefix s ~prefix:"." -> Number ("0" ^ s)
-     | s -> Number s)
-  <?> "number"
+  return (Number (sign ^ int_part ^ decimal_part)) <?> "number"
 ;;
 
 let symbol =
-  (take_till @@ fun ch -> Char.is_whitespace ch)
-  >>| fun str -> Symbol str
+  take_while1 (fun ch -> not (is_delim ch))
+  >>| (fun str -> Symbol str)
+  <?> "symbol"
 ;;
 
-let sexp =
+(* -------------------------------------------------------------------------- *)
+(* Recursive S-expression grammar *)
+(* -------------------------------------------------------------------------- *)
+let sexp : sexpr Angstrom.t =
   fix (fun sexp ->
     let list_rest =
       fix (fun list_rest ->
         choice
           [ (* end of list *)
-            char ')' *> return []
+            token (char ')') *> return Null
+          ; (* dotted pair *)
+            (let* _ = token (char '.') in
+             let* tail = sexp in
+             let* _ = token (char ')') in
+             return tail)
           ; (* recursive list elements *)
-            (let* x = sexp in
+            (let* x = token sexp in
              let* xs = list_rest in
-             return (x :: xs))
+             return (Cons (x, xs)))
           ])
     in
-    let list = char '(' *> list_rest >>| fun xs -> List xs in
-    choice [ number; symbol; list ])
+    let list = token (char '(') *> list_rest <?> "list" in
+    token (choice [ list; number; symbol ]))
+  <?> "sexp"
 ;;
 
-(* choice *)
-(*   [ char ')' *> List [] *)
-(*   ; List (char '.' *> sexp <* char ')') *)
-(*   ; List (List.append sexp list_rest) *)
-(*   ] *)
+let parse_and_print input =
+  match parse_string ~consume:All sexp input with
+  | Ok result ->
+    Stdio.printf "Parsed: %s\n%!" (show_sexpr result)
+  | Error msg -> Stdio.printf "Error: %s\n%!" msg
+;;
 
 let () =
-  let open Stdlib.Printf in
-  let input = ".1" in
-  match parse_string ~consume:All sexp input with
-  | Ok result -> printf "%s" (show_sexpr result)
-  | Error msg -> printf "%s" msg
+  parse_and_print "(+ (1 3))";
+  parse_and_print "(a b . c)";
+  parse_and_print "(1 (2 3) 4)";
+  parse_and_print "(x)";
+  parse_and_print "()"
 ;;
